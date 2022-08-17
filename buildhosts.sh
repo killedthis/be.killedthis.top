7540738b89d0d15fd830d5107ea6e62d1943a529
earentir <earentir@gmail.com>
2022-08-17 23:22:00 +0300

    minor print statement fixes

--- buildhosts.sh
+++ buildhosts.sh
@@ -4,11 +4,10 @@
 source /home/www/secret/r53
 source /home/www/secret/db
 
-# hostnames=$(mysql -u$DBUSERNAME -p$DBPASSWORD -h dbserver -D funny -e "SELECT serviceprovider FROM killedthis" | rev | rev | tail -n +2 | sort -u | uniq)
-
 readarray -t SERVICES < <(mysql -u"$DBUSERNAME" -p"$DBPASSWORD" -h dbserver -D funny -e "SELECT serviceprovider FROM killedthis" | rev | rev | tail -n +2 | sort -u | uniq)
 
-printf "Hostnames Found %s \n" "${SERVICES[@]}"
+printf "Service Providers Found %s \n" "${SERVICES[@]}"
+echo -e
 
 for SERVICE in "${SERVICES[@]}"; do
 	hostnamelist="${hostnamelist} ${SERVICE,}.killedthis.top"
@@ -19,9 +18,9 @@ done
 printf "server {\n\tlisten 80;\n\tserver_name%s;\n\n\treturn 301 https://\$server_name\$request_uri;\n\n\tlocation ~ /\.(?!well-known).* {\n\t\tdeny all;\n\t}\n\n}\n\n" "$hostnamelist" >/home/www/hosts/dynamicservers.conf
 
 for hostname in "${SERVICES[@]}"; do
-	/home/www//r53b/r53b "${hostname,}"
+	/home/www/r53b/r53b "${hostname,}"
 	printf "server {\n\tlisten 443 ssl http2;\n\tserver_name %s.killedthis.top;\n\tinclude /home/www/hosts/common/dynamicvhost.conf;\n\n\tlocation / {\n\t\troot /home/www/sites/killedthis.top/;\n\t\tindex \$server_name.html;\n\t}\n\n\tlocation ~ /\.(?!well-known).* {\n\t\tdeny all;\n\t}\n\n}\n" "${hostname,}" >>/home/www/hosts/dynamicservers.conf
-	printf "%s\n" "${hostname,}"
+	echo -e
 done
 
 if nginx -t; then

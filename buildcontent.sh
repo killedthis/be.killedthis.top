a9351801de8f293a8926d3547515de4129e92180
earentir <earentir@gmail.com>
2022-08-18 08:28:07 +0300

    get tmdb ids from DB if we have an id we pull the postel

--- buildcontent.sh
+++ buildcontent.sh
@@ -13,6 +13,7 @@ for SERVICE in "${SERVICES[@]}"; do
     readarray -t YEARS < <(mysql -u"$DBUSERNAME" -p"$DBPASSWORD" -h dbserver -D funny -e "SELECT date FROM killedthis WHERE serviceprovider='$SERVICE'" | rev | rev | tail -n +2 | cut -c -4 | sort -s | uniq)
     readarray -t MONTHS < <(mysql -u"$DBUSERNAME" -p"$DBPASSWORD" -h dbserver -D funny -e "SELECT date FROM killedthis WHERE serviceprovider='$SERVICE'" | rev | rev | tail -n +2 | cut -c 6-7 | sort -s | uniq)
     readarray -t DATEOFDEATH < <(mysql -u"$DBUSERNAME" -p"$DBPASSWORD" -h dbserver -D funny -e "SELECT date FROM killedthis WHERE serviceprovider='$SERVICE'" | rev | rev | tail -n +2)
+    readarray -t TMDBID < <(mysql -u"$DBUSERNAME" -p"$DBPASSWORD" -h dbserver -D funny -e "SELECT tmdbid FROM killedthis WHERE serviceprovider='$SERVICE'" | rev | rev | tail -n +2)
 
     YEARFILTER=""
     for INDEX in "${!YEARS[@]}"; do
@@ -26,7 +27,15 @@ for SERVICE in "${SERVICES[@]}"; do
 
     TITLELIST=""
     for INDEX in "${!TITLES[@]}"; do
-        TITLELIST="${TITLELIST} <li class=\"kt-item\" year=\"${DATEOFDEATH[$INDEX]::-6}\" month=\"${DATEOFDEATH[$INDEX]:5:2}\" tmdbid=\"\"><img src=\"img/cbimage.jpg\" class=\"kt-thumbnail\" alt=\"No movie thumbnail\"><div class=\"uk-text-secondary kt-item-title\">${TITLES[$INDEX]}</div><div class=\"kt-item-date\">${DATEOFDEATH[$INDEX]::-3}</div></li>"
+        if [ "${TMDBID[$INDEX]}" == "NULL" ]; then
+            IMAGENAME="img/cbimage.jpg"
+            /home/www/tmdb/tmdb -s ${TITLES[$INDEX]}
+        else
+            /home/www/tmdb/tmdb -t ${TMDBID[$INDEX]}
+            IMAGENAME="img/posters/${TMDBID[$INDEX]}.jpg"
+        fi
+
+        TITLELIST="${TITLELIST} <li class=\"kt-item\" year=\"${DATEOFDEATH[$INDEX]::-6}\" month=\"${DATEOFDEATH[$INDEX]:5:2}\" tmdbid=\"${TMDBID[$INDEX]}\"><img src=\"$IMAGENAME\" class=\"kt-thumbnail\" alt=\"No movie thumbnail\"><div class=\"uk-text-secondary kt-item-title\">${TITLES[$INDEX]}</div><div class=\"kt-item-date\">${DATEOFDEATH[$INDEX]::-3}</div></li>"
     done
 
     cp /home/www/sites/killedthis.top/site.template /home/www/sites/killedthis.top/"${SERVICE,}".killedthis.top.html

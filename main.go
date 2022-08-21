7d6f4b1b5bdb99dd97b1e9bdfadfecad7bc90acd
Johnny Wulgaru <wulgaru@hispeed.ch>
2022-08-21 17:30:15 +0200

    added some logging

--- main.go
+++ main.go
@@ -14,18 +14,20 @@ func main() {
 	}
 
 	// Get all Service Providers
+	log.Println("Retrieving service providers...")
 	serviceProviders := database.GetServiceProviders()
 	if serviceProviders == nil {
 		log.Panic("nothing found")
 	}
 
 	// Render the templates for each service provider
-	for _, x := range serviceProviders {
+	for _, provider := range serviceProviders {
+		log.Printf("Rendering Page for Provider '%s'...\n", provider)
 		// get the shows killed by this provider
-		shows := database.GetShowsByProvider(x)
+		shows := database.GetShowsByProvider(provider)
 
 		// create a new renderer for this provider and shows
-		renderer := builder.NewRenderer(x, serviceProviders, shows)
+		renderer := builder.NewRenderer(provider, serviceProviders, shows)
 
 		// render it
 		renderer.RenderHtml()

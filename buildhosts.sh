00f0089e471d144ffd46658bb40491c3fe33cc06
earentir <earentir@gmail.com>
2024-01-10 23:50:59 +0200

    added changes to support http3

--- buildhosts.sh
+++ buildhosts.sh
@@ -21,7 +21,10 @@ for hostname in "${SERVICES[@]}"; do
 		cd /home/www/golang/r53b || exit
 		/home/www/golang/r53b/r53b.go "${hostname,}"
 	)
-	printf "server {\n\tlisten 443 ssl;\n\thttp2 on;\n\tserver_name %s.killedthis.top;\n\tinclude /home/www/hosts/common/dynamicvhost.conf;\n\n\tlocation / {\n\t\troot /home/www/sites/killedthis.top/;\n\t\tindex \$server_name.html;\n\t}\n\n\tlocation ~ /\.(?!well-known).* {\n\t\tdeny all;\n\t}\n\n}\n" "${hostname,}" >>/home/www/hosts/dynamicservers.conf
+
+
+	printf "server {\n\tlisten 443 quic;\n\tlisten 443 ssl;\n\tlisten [::]:443 quic;\n\tlisten [::]:443 ssl;\n\thttp2 on;\n\n\t
+	server_name %s.killedthis.top;\n\tinclude /home/www/hosts/common/dynamicvhost.conf;\n\n\tlocation / {\n\t\troot /home/www/sites/killedthis.top/;\n\t\tindex \$server_name.html;\n\t}\n\n\tlocation ~ /\.(?!well-known).* {\n\t\tdeny all;\n\t}\n\n}\n" "${hostname,}" >>/home/www/hosts/dynamicservers.conf
 	echo -e
 done
 

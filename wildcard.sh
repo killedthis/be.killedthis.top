96f80e067816c75dde51941bcaefe0c7f1702026
earentir <earentir@gmail.com>
2024-01-10 22:58:26 +0200

    added a line for my other domain

--- wildcard.sh
+++ wildcard.sh
@@ -1,4 +1,6 @@
 source /home/www/secret/r53
 lego -d killedthis.top -d *.killedthis.top --email="admin@ear.pm" --dns route53 run
 
+lego -d earentir.dev -d *.earentir.dev --email="admin@ear.pm" --dns route53 run
+
 echo "key in .lego/certificates"
